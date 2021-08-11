29e1c222a6404ec1eda40a2cf8811ea5c6a87ecd
KADYAN <35776003+unique-kadyan@users.noreply.github.com>
2021-08-11 18:19:57 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ class App extends Component {
 	};
    
    
-    }
+    
  renderPara(){
    return(
 	   <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
