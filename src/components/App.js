1966f004ab5538c03a2e2970216f3df38c8bc721
KADYAN <35776003+unique-kadyan@users.noreply.github.com>
2021-08-11 16:58:14 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,16 @@ class App extends Component {
     constructor(props) {
 		super(props);
 	};
+    paraFunction(){
+        console.log("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy");
 
+    }
     render() {
     	return(
     		<div id="main">
 				{ /* Do not remove this main div!! */ }
+                <p id="para"></p>
+	<button id="click" onclick={paraFunction}> Click Me</button>
     		</div>
     	);
     }
@@ -17,4 +22,3 @@ class App extends Component {
 
 
 export default App;
-
